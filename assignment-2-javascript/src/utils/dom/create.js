da37e15192c1aae865ee5f27efd3da3bb90d4908
Hououin Kyouma <inwazzin@gmail.com>
2022-03-20 20:39:20 +0100

    create style object

--- assignment-2-javascript/src/utils/dom/create.js
+++ assignment-2-javascript/src/utils/dom/create.js
@@ -1,9 +1,26 @@
+import { cx } from "./cx.js";
+
+/**
+ * @typedef Props
+ * @type {object}
+ * @property {string=} [class] - class names
+ * @property {CSSStyleDeclaration=} [style] - style object
+ * */
+
+/** @param style {CSSStyleDeclaration} */
+export const styled = (style) =>
+  Object.entries(style)
+    .map(([k, v]) => `${k}:${v}`)
+    .join(";")
+    .replace(/[A-Z]/g, (match) => `-${match.toLowerCase()}`);
+
 /**
  * @param tag {string} - The tag name of the element to create.
- * @param props {object=} - optional tag properties.
+ * @param props {Props=} - optional properties.
  * @return {HTMLElement} */
 export const create = (tag, props = {}) =>
   Object.assign(document.createElement(tag), {
-    className: props.class,
     ...props,
+    className: cx(props.class, props.className),
+    style: props.style && styled(props.style),
   });
