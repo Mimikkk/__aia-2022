ab21bb548a015e73ab895b086dc400034048cd80
Hououin Kyouma <inwazzin@gmail.com>
2022-03-20 19:44:53 +0100

    add dom utility

---
+++ assignment-2-javascript/src/utils/dom/add.js
@@ -0,0 +1,52 @@
+/**
+ * @typedef NodeElement
+ * @type {string | number | undefined | HTMLElement | (() => NodeElement)}
+ * */
+
+/**
+ * @param parent {HTMLElement | undefined}
+ * @param children {NodeElement | NodeElement[]}
+ * */
+export const addChild = (parent, children) => {
+  if (!parent || !children) return;
+
+  switch (typeof children) {
+    case "string":
+    case "number":
+      return (parent.textContent = `${parent?.textContent}${children}`);
+    case "function":
+      return parent.appendChild(children());
+    case "object":
+      switch (true) {
+        case children instanceof Array:
+          return children.forEach((child) => addChild(parent, child));
+        case children instanceof HTMLElement:
+          return parent.appendChild(children);
+        default:
+          throw Error(`Unknown child instance ${children}`);
+      }
+    default:
+      throw Error(`Unknown child type ${children}`);
+  }
+};
+/**
+ * @param parent {HTMLElement | undefined}
+ * @param children {NodeElement | NodeElement[]}
+ * */
+export const add = (parent, ...children) => {
+  if (!parent || !children) return;
+
+  children.reduce((parent, child) => {
+    if (!parent || !child) return;
+
+    switch (typeof child) {
+      case "function":
+        const child_ = child();
+        addChild(parent, child_);
+        return child_;
+      default:
+        addChild(parent, child);
+        return child;
+    }
+  }, parent);
+};
