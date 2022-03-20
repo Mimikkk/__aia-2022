bacc39f804d753f40cae5d3cb6ecb8822708e2d4
Hououin Kyouma <inwazzin@gmail.com>
2022-03-20 20:51:33 +0100

    outlined box

---
+++ assignment-2-javascript/src/components/OutlineBox/OutlineBox.js
@@ -0,0 +1,23 @@
+import { add, cx } from "../../utils";
+import { fieldset, legend } from "../DOM";
+import { loadCss } from "../../styles/load.js";
+
+loadCss("src/components/OutlineBox/OutlineBox.css");
+
+/** @param label {string | undefined}
+ * @param centered {boolean | undefined}
+ * @param className {string | undefined}
+ * @return {HTMLFieldSetElement}
+ * */
+export const OutlineBox = ({ className, label, centered = true } = {}) =>
+  add(
+    fieldset({
+      className: cx(
+        "outline-box",
+        label && "labeled",
+        centered && "centered",
+        className
+      ),
+    }),
+    label && legend({ label })
+  );
