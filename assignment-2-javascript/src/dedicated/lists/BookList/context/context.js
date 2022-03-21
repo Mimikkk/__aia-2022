47cb5592301d33caf0401948d7ccef057fe1a032
Hououin Kyouma <inwazzin@gmail.com>
2022-03-21 22:50:55 +0100

    book collection context

--- assignment-2-javascript/src/dedicated/lists/BookList/context/context.js
+++ assignment-2-javascript/src/dedicated/lists/BookList/context/context.js
@@ -1,1 +1,50 @@
-export const bookContext = () => {};
+import { add, replace } from "../../../../utils";
+import { ActionGroup } from "../components";
+import { input, td } from "../../../../components/DOM";
+
+export const Textfield = (value) =>
+  input({
+    type: "text",
+    value: value || "",
+    maxLength: 36,
+    class: "text-field",
+  });
+
+const context = {
+  /** @type {HTMLTableElement}*/
+  ref: null,
+
+  add: () => {
+    const [body] = context.ref.getElementsByTagName("tbody");
+    const row = body.insertRow();
+
+    add(row, [
+      add(td, Textfield),
+      add(td, Textfield),
+      add(td, ActionGroup({ row, save: true, remove: true })),
+    ]);
+  },
+
+  /**@param row {HTMLTableRowElement}*/
+  save: (row) => () => {
+    const [author, title, controls] = row.getElementsByTagName("td");
+
+    replace(author, author.firstChild.value || "No Author");
+    replace(title, title.firstChild.value || "No Title");
+    replace(controls, ActionGroup({ row, edit: true, remove: true }));
+  },
+
+  /**@param row {HTMLTableRowElement}*/
+  edit: (row) => () => {
+    const [author, title, controls] = row.getElementsByTagName("td");
+
+    replace(author, Textfield(author.textContent));
+    replace(title, Textfield(title.textContent));
+    replace(controls, ActionGroup({ row, save: true, remove: true }));
+  },
+
+  /**@param row {HTMLTableRowElement}*/
+  remove: (row) => () => context.ref.deleteRow(row.rowIndex),
+};
+
+export { context as bookContext };
